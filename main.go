0df2edc9c8a8e3ca5da177d7b4c01cb2d166c135
Atharva Pardeshi <atharva.exe@gmail.com>
2024-04-07 10:45:59 +0530

    feat: serve static files on "/app" endpoint from root "."

--- main.go
+++ main.go
@@ -1,33 +1,31 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
 )
 
 func main() {
+	const filepathRoot = "."
+	const port = "8080"
+
 	mux := http.NewServeMux()
-	corsMux := middleWareCors(mux)
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.HandleFunc("/healthz", handleReadiness)
+
+	corsMux := middlewareCors(mux)
+
 	server := http.Server{
+		Addr:    ":" + port,
 		Handler: corsMux,
-		Addr: ":8080",
 	}
 
-	err := server.ListenAndServe()
-	if err != nil {
-		fmt.Println(err)
-	}
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(server.ListenAndServe())
 }
 
-func middleWareCors(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "*")
-		if r.Method == "OPTIONS" {
-			w.WriteHeader(http.StatusOK)
-			return
-		}
-		next.ServeHTTP(w, r)
-	})
+func handleReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
